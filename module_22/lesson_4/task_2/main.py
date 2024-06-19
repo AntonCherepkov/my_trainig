c2dd6e7cb923cf8181bda40e74b5bdf2cb196d14
Anton Cherepkov <phantomnbdg2@gmail.ru>
2024-06-19 16:21:41 +0500

    write functions in main.py

--- module_22/lesson_4/task_2/main.py
+++ module_22/lesson_4/task_2/main.py
@@ -1,1 +1,30 @@
-# Здесь буду писать свой код
+import os
+
+
+def count_letters(word):
+    return {
+        letter: sum(1 for i_letter in word if i_letter == letter)
+        for letter in word
+    }
+
+
+def is_palindrome(count_dict):
+    numb_even_grouper = 0
+    numb_odd_grouper = 0
+    for letter, counter in count_dict.items():
+        if counter % 2 == 0:
+            numb_even_grouper += 1
+        else:
+            numb_odd_grouper += 1
+    if numb_odd_grouper > 1:
+        return False
+    else:
+        return True
+
+
+for word in 'asdddsa', 'adddaaghkkll':
+    count_let = count_letters(word)
+    print(is_palindrome(count_let))
+
+
+
